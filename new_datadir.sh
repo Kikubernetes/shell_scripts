9012a672f1a471669377fc6cc0f1b022b2a0a1c7
Kikubernetes <kikukikukiku2006@yahoo.co.jp>
2023-07-30 11:08:03 +0900

    fixed

--- new_datadir.sh
+++ new_datadir.sh
@@ -5,15 +5,16 @@
 # Written by Kikuko K at 20230724.
 
 ###---------------Variables------------------###
-original_directory=/media/kikuko/kotatsu/SR/data
-new_directory=/media/kikuko/kotatsu/SR/syn_data
+original_directory=path_to_the_original_data_directory
+new_directory=path_to_the_new_data_directory
+f_and_d=file_name or directory_name
 ################################################
 
 sublist=$(ls $original_directory)
 
 for sub in $sublist; do
     mkdir -p $new_directory/$sub
-    cp -r $original_directory/$sub/nifti_data $new_directory/$sub
+    cp -r $original_directory/$sub/$f_and_d $new_directory/$sub
 
 done
 
